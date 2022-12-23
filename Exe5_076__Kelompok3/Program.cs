f04b410f682537517371039019bef1b6a6ca5d83
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-12-23 16:28:23 +0700

    add variable key and next

--- Exe5_076__Kelompok3/Program.cs
+++ Exe5_076__Kelompok3/Program.cs
@@ -5,6 +5,15 @@ namespace Exe5
     // A linked list (LL) node to store a queue entry
     class QNode
     {
-
+        // Create variable key and next
+        public string key;
+        public QNode next;
+        // constructor to create a new linked list node
+        public QNode(string key)
+        {
+            this.key = key;
+            this.next = null;
+        }
     }
+
 }
